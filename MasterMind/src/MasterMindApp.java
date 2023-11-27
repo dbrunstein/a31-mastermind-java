c00a078fbd1ce9e4993fe304d80eaf1e0d799778
Damien BRUNSTEIN <dbrunstein@unistra.fr>
2023-11-27 17:23:15 +0100

    edit main base constructeur

--- MasterMind/src/MasterMindApp.java
+++ MasterMind/src/MasterMindApp.java
@@ -1,8 +1,11 @@
+import Controller.MasterController;
 import Model.MasterMindGame;
+import View.StartWindow;
 
 public class MasterMindApp {
     public static void main(String[] args) {
         MasterMindGame game = new MasterMindGame();
-
+        MasterController controller = new MasterController(game);
+        StartWindow mainWindow = new StartWindow(controller);
     }
 }
