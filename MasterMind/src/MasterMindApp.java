be18255715a83afd13fd5dd049ff79fdb9f917b3
Damien BRUNSTEIN <dbrunstein@unistra.fr>
2023-11-27 17:30:55 +0100

    Revert "edit main base constructeur"
    This reverts commit c00a078fbd1ce9e4993fe304d80eaf1e0d799778.

--- MasterMind/src/MasterMindApp.java
+++ MasterMind/src/MasterMindApp.java
@@ -1,11 +1,8 @@
-import Controller.MasterController;
 import Model.MasterMindGame;
-import View.StartWindow;
 
 public class MasterMindApp {
     public static void main(String[] args) {
         MasterMindGame game = new MasterMindGame();
-        MasterController controller = new MasterController(game);
-        StartWindow mainWindow = new StartWindow(controller);
+
     }
 }
