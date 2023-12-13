5c8629eb70cbac1bdbc3171cfb0bed97936670e3
Damien BRUNSTEIN <dbrunstein@unistra.fr>
2023-12-13 16:34:55 +0100

    main stable, do not touch

--- MasterMind/src/MasterMindApp.java
+++ MasterMind/src/MasterMindApp.java
@@ -1,15 +1,19 @@
 import Controller.MasterController;
 import Model.Board;
+import Model.Combination;
 import Model.Player;
 import View.StartWindow;
 
 public class MasterMindApp {
     public static void main(String[] args) {
         Player player = new Player();
+        Combination coom = new Combination();
+        coom.createCombination(4);
+        player.setCombination(coom);
         Board board = new Board(player);
         MasterController masterController = new MasterController(board);
         StartWindow startWindow = new StartWindow(masterController);
-        //board.startNewGame();
+        board.startNewGame();
         System.out.println("MasterMindApp started");
     }
 }
